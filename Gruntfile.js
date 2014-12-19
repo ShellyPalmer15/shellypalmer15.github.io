9400c6269637fde1c2d906c96bbdfd2702e3239b
Jared Palmer <palmdeezy>
2014-12-19 14:05:43 -0500

    remove check for deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -452,7 +452,7 @@ module.exports = function(grunt) {
             ]);
 
             grunt.registerTask('deploy', [
-                'check',
+                // 'check',
                 'test',
                 'build',
                 'buildcontrol'
