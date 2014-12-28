afd85701d709ea2acc30c9b4431f7be3b54d245f
Jared Palmer <palmdeezy>
2014-12-28 16:09:57 -0500

    stop image min for a moment

--- Gruntfile.js
+++ Gruntfile.js
@@ -444,7 +444,7 @@ module.exports = function(grunt) {
                 'autoprefixer:dist',
                 'cssmin',
                 'uglify',
-                'imagemin',
+                // 'imagemin',
                 'svgmin',
                 'filerev',
                 'usemin',
