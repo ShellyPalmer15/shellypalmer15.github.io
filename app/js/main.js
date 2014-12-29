a0252dfaa4390fd2bf3ec96a3098bcc146944d25
Jared Palmer <palmdeezy>
2014-12-29 15:19:53 -0500

    yup

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-  $(".lazy").unveil(200, function() {
-  $(this).load(function() {
-    this.style.opacity = 1;
-  });
-});
+// $(document).ready(function() {
+//   $(".lazy").unveil(200, function() {
+//   $(this).load(function() {
+//     this.style.opacity = 1;
+//   });
+// });
 
-(function() {
-    'use strict';
-    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-        var msViewportStyle = document.createElement('style')
-        msViewportStyle.appendChild(
-            document.createTextNode(
-                '@-ms-viewport{width:auto!important}'
-            )
-        )
-        document.querySelector('head').appendChild(msViewportStyle)
-    }
-})();
+// (function() {
+//     'use strict';
+//     if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+//         var msViewportStyle = document.createElement('style')
+//         msViewportStyle.appendChild(
+//             document.createTextNode(
+//                 '@-ms-viewport{width:auto!important}'
+//             )
+//         )
+//         document.querySelector('head').appendChild(msViewportStyle)
+//     }
+// })();
