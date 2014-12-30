84919a535b2cf6a3a8cb793f2acc4a089aa667be
Jared Palmer <palmdeezy>
2014-12-30 10:21:05 -0500

    fixed filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -455,7 +455,7 @@ module.exports = function(grunt) {
                 'autoprefixer:dist',
                 'cssmin',
                 'uglify',
-                'imagemin',
+                // 'imagemin',
                 'svgmin',
                 'filerev',
                 'usemin',
