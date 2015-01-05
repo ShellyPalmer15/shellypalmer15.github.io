6d7abfdb2149b290e8d3f7b6fda9d1382ddc32e7
Jared Palmer <palmdeezy>
2015-01-04 23:56:52 -0500

    browsersynce

--- Gruntfile.js
+++ Gruntfile.js
@@ -416,7 +416,7 @@ module.exports = function(grunt) {
                     'concurrent:server',
                     'autoprefixer:server',
                     'connect:livereload',
-                    // 'browserSync',
+                    'browserSync',
                     'watch'
 
                 ]);
